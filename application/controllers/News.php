3d4972f6f309085957795fcfb6ef476ee290b27c
CC122@ccs.local <CC122@ccs.local>
2019-08-04 05:26:00 +0800

    static fina

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -58,18 +58,5 @@ class News extends CI_Controller {
     }
     }
 
-    public function set_news()
-    {
-    $this->load->helper('url');
-
-    $slug = url_title($this->input->post('title'), 'dash', TRUE);
-
-    $data = array(
-        'title' => $this->input->post('title'),
-        'slug' => $slug,
-        'text' => $this->input->post('text')
-    );
-
-    return $this->db->insert('news', $data);
-    }
+    
 }
