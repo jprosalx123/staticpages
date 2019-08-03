fdd1f91671102fed28570720a255dccfe8cd06c1
CC122@ccs.local <CC122@ccs.local>
2019-08-04 04:41:15 +0800

    static pages

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -33,4 +33,43 @@ class News extends CI_Controller {
                 $this->load->view('news/view', $data);
                 $this->load->view('templates/footer');
         }
+        
+        public function create()
+    {
+    $this->load->helper('form');
+    $this->load->library('form_validation');
+
+    $data['title'] = 'Create a news item';
+
+    $this->form_validation->set_rules('title', 'Title', 'required');
+    $this->form_validation->set_rules('text', 'Text', 'required');
+
+    if ($this->form_validation->run() === FALSE)
+    {
+        $this->load->view('templates/header', $data);
+        $this->load->view('news/create');
+        $this->load->view('templates/footer');
+
+    }
+    else
+    {
+        $this->news_model->set_news();
+        $this->load->view('news/success');
+    }
+    }
+
+    public function set_news()
+    {
+    $this->load->helper('url');
+
+    $slug = url_title($this->input->post('title'), 'dash', TRUE);
+
+    $data = array(
+        'title' => $this->input->post('title'),
+        'slug' => $slug,
+        'text' => $this->input->post('text')
+    );
+
+    return $this->db->insert('news', $data);
+    }
 }
